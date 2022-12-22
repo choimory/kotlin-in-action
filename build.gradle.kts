08572ed0dc8a381f3e0136278fa7f6c0d824599d
choimory <choimory@naver.com>
2022-12-22 17:54:32 +0900

    chore: fix gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
     id("org.asciidoctor.convert") version "1.5.8"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.6.21"
-    id("org.jlleitschuh.gradle.ktlint") version "9.1.0"
+    //id("org.jlleitschuh.gradle.ktlint") version "9.1.0"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     kotlin("plugin.jpa") version "1.6.21"
@@ -15,7 +15,7 @@ allOpen {
     //final이 되어선 안되는 특정 클래스에 활용하기 위한 커스텀 어노테이션
     annotation("com.choimory.kotlininaction.common.annotation.AllOpen")
 
-    //Hibernate의 Entity class 및 instance field는 final이 되어선 안된다 - 공식문서
+    //Hibernate의 Entity class 및 instance field는 final이 되어선 안된다 (프록시 패턴으로 동작하기 때문에) - 공식문서
     annotation("javax.persistence.Entity")
     annotation("javax.persistence.MappedSuperclass")
     annotation("javax.persistence.Embeddable")
@@ -48,11 +48,12 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     compileOnly("org.projectlombok:lombok")
-    //developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
